9179d9823bd2e33e62f22fbecd288e640fd2e3fb
Yusuf Matra <yusufmatra@Yusufs-Mac-mini.local>
2026-08-10 21:32:28 +0700

    Update tugas sambil belajar git push dan commit

--- backend/main.py
+++ backend/main.py
@@ -62,6 +62,41 @@ print_trip_summary(
 
 
 # Codingan pertemuan 1
+
+# def print_trip_summary(
+#     destination,
+#     days,
+#     budget,
+#     travel_style,
+#     hotel_cost,
+#     food_cost,
+#     transportation_cost,
+#     currency,
+#     country,
+#     travel_month,
+#     miscellaneous_cost,
+# ):
+#     total_estimated_cost = hotel_cost + food_cost + transportation_cost + miscellaneous_cost
+
+#     print("\n===== TRIP SUMMARY KELANA AI =====")
+#     print(f"Destination : {destination}")
+#     print(f"Country     : {country}")
+#     print(f"Days        : {days}")
+#     print(f"Budget      : {budget} {currency}")
+#     print(f"Travel Style: {travel_style}")
+#     print(f"Hotel Cost  : {hotel_cost} {currency}")
+#     print(f"Food Cost   : {food_cost} {currency}")
+#     print(f"Transportation Cost: {transportation_cost} {currency}")
+#     print(f"Miscellaneous Cost: {miscellaneous_cost} {currency}")
+#     print(f"Total Estimated Cost: {total_estimated_cost} {currency}")
+#     print(f"Travel Month: {travel_month}")
+#     print ("==================================")
+
+#     if total_estimated_cost > budget:
+#         print("⚠️ Budget Exceeded")
+
+#     print()
+
 # print_trip_summary(
 #     destination="Japan",
 #     days=5,

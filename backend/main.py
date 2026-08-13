d0db15519f144e6318dc43f652a58d6a3ff31bad
Yusuf Matra <yusufmatra@Yusufs-Mac-mini.local>
2026-08-13 16:19:33 +0700

    Tugas Sesi 2: Making KelanaAI Smarter

--- backend/main.py
+++ backend/main.py
@@ -1,34 +1,46 @@
-from services.trip_service import calculate_daily_budget, get_trip_category
+from services.trip_service import (
+    get_trip_category,
+    get_travel_season,
+    calculate_daily_budget,
+    get_recommended_places
+)
+
 
 destination = input("Enter Destination: ")
-country = input("Destination Country: ")
 days = int(input("Many Days: "))
 budget = float(input("Your Budget: "))
 currency = input("Currency: ")
 travel_month = input("Travel Month: ")
 
+
 def print_trip_summary(
     destination,
-    country,
     days,
     budget,
     currency,
     travel_month
 ):
-    print("\n===== TRIP SUMMARY KELANA AI =====")
-    print(f"Destination         : {destination}")
-    print(f"Country             : {country}")
-    print(f"Days                : {days}")
-    print(f"Category            : {get_trip_category(budget)}")
-    print(f"Daily Budget        : {calculate_daily_budget(budget, days)} {currency}/day")
-    print(f"Budget              : {budget} {currency}")
-    print(f"Currency            : {currency}")
-    print(f"Travel Month        : {travel_month}")
-    print("===================================")
+    print()
+    print("==================================")
+    print("KelanaAI")
+    print("==================================")
+    print(f"Destination     : {destination}")
+    print(f"Days            : {days}")
+    print(f"Budget          : {budget} {currency}")
+    print(f"Category        : {get_trip_category(budget)}")
+    print(f"Daily Budget    : {calculate_daily_budget(budget, days)} {currency}/Day")
+    print(f"Travel Month    : {travel_month}")
+    print(f"Season          : {get_travel_season(travel_month)}")
+    print("==================================")
+    print("Recommended Places")
+    print("----------------------------------")
+    get_recommended_places()
+    print("----------------------------------")
+    print()
+
 
 print_trip_summary(
     destination,
-    country,
     days,
     budget,
     currency,
--- backend/services/trip_service.py
+++ backend/services/trip_service.py
@@ -1,11 +1,34 @@
-# Sesi 2 - Daily Budget Calculation
-def calculate_daily_budget(budget, days):
-    return budget / days
-
+# Kondisi untuk menentukan kategori perjalanan berdasarkan anggaran
 def get_trip_category(budget):
     if budget < 1000:
         return "Backpacker"
-    elif 1000 <= budget < 5000:
+    elif budget <= 3000:
         return "Standard"
     else:
         return "Luxury"
+
+# Kondisi untuk menentukan musim perjalanan berdasarkan bulan
+def get_travel_season(month):
+    if month == "December":
+        return "Peak Season"
+    elif month == "June":
+        return "Holiday Season"
+    else:
+        return "Regular Season"
+
+# Fungsi untuk menghitung budget harian berdasarkan total budget dan jumlah hari
+def calculate_daily_budget(budget, days):
+    return budget / days
+
+# Fungsi list untuk mendapatkan daftar tempat yang direkomendasikan
+def get_recommended_places():
+    recommended_places = [
+        "Tokyo Tower",
+        "Shibuya",
+        "Mount Fuji"
+    ]
+
+    for place in recommended_places:
+        print(f"- {place}")
+
+

c2ecb9a492501378bdc2c9450be8d6935fbb930c
Yusuf Matra <yusufmatra@Yusufs-Mac-mini.local>
2026-08-13 14:14:20 +0700

    Perbaiki tugas sesi 1 agar lebih rapih

--- backend/main.py
+++ backend/main.py
@@ -7,26 +7,27 @@ travel_month = input("Travel Month: ")
 
 def print_trip_summary(
     destination,
+    country,
     days,
     budget,
     currency,
-    country,
-    travel_month,
+    travel_month
 ):
     print("\n===== TRIP SUMMARY KELANA AI =====")
     print(f"Destination         : {destination}")
     print(f"Country             : {country}")
     print(f"Days                : {days}")
     print(f"Budget              : {budget} {currency}")
+    print(f"Currency            : {currency}")
     print(f"Travel Month        : {travel_month}")
     print ("=====================================")
 
 print_trip_summary(
-        destination,
-        days,
-        budget,
-        currency,
-        country,
-        travel_month,
-)   
+    destination,
+    country,
+    days,
+    budget,
+    currency,
+    travel_month
+)
 

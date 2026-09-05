3b2778017b001929ed0f6086bfa6c464a2655a4e
Yusuf Matra <yusufmatra@Yusufs-Mac-mini.local>
2026-09-05 17:37:46 +0700

    allow vercel frontend in cors

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://kelana-ai-theta.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

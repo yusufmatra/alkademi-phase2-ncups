fd3067c411f5ec4e1075bccaf677b9fe157c5fa3
Yusuf Matra <yusufmatra@Yusufs-Mac-mini.local>
2026-09-05 19:53:10 +0700

    update main.py lagi

--- backend/main.py
+++ backend/main.py
@@ -26,6 +26,7 @@ app.add_middleware(
         "http://127.0.0.1:3000", 
         "https://kelana-ai-theta.vercel.app",
         ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
